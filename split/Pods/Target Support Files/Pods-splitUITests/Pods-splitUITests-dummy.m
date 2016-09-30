60da67777f6758bae193497b47f0cf3f0bf63007
Elaine Guo <YilinGUO>
2016-09-30 00:10:35 -0400

    initial commit

---
+++ split/Pods/Target Support Files/Pods-splitUITests/Pods-splitUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_splitUITests : NSObject
+@end
+@implementation PodsDummy_Pods_splitUITests
+@end
---
+++ split/Pods/Target Support Files/Pods-splitUITests/Pods-splitUITests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_splitUITestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_splitUITestsVersionString[];
+
