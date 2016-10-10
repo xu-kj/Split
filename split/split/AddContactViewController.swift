61ac99e2dce4f998d71c67898a3a0cdac8c5a564
Shuo Chen <shuochen@umich.edu>
2016-10-10 00:18:45 -0400

    add contact

--- split/split/AddContactViewController.swift
+++ split/split/AddContactViewController.swift
@@ -8,45 +8,18 @@
 
 import UIKit
 
-class AddContactViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate {
-
-    @IBOutlet weak var tableView: UITableView!
+class AddContactViewController: UIViewController,  UITextFieldDelegate {
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "contactCell", for: indexPath as IndexPath)
-        let label : UILabel = self.view.viewWithTag(1) as! UILabel
-        let textField : UITextField = self.view.viewWithTag(2) as! UITextField
-        if indexPath.row == 0 {
-            label.text = "Name"
-            textField.keyboardType = UIKeyboardType.namePhonePad
-        } else if indexPath.row == 1 {
-            label.text = "Mobile"
-            textField.keyboardType = UIKeyboardType.numberPad
-            let toolBar = UIToolbar(frame: CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: 50))
-            toolBar.barStyle = UIBarStyle.default
-            toolBar.items = [
-                UIBarButtonItem(title: "Cancel", style: UIBarButtonItemStyle.plain, target: self, action: #selector(self.keyboardCancelButtonTapped(_:))),
-                UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.flexibleSpace, target: nil, action: nil),
-                UIBarButtonItem(title: "Done", style: UIBarButtonItemStyle.plain, target: self, action: #selector(self.keyboardDoneButtonTapped(_:)))]
-            toolBar.sizeToFit()
-            textField.inputAccessoryView = toolBar
-        } else if indexPath.row == 2 {
-            label.text = "Email"
-            textField.keyboardType = UIKeyboardType.emailAddress
-        }
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 55.0
-    }
-    
-    func saveContact(_ sender: UIButton) {
-        print("click")
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var mobileTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var saveButton: UIButton!
+    @IBAction func saveButtonClicked(_ sender: AnyObject) {
+        print("save button clicked")
+        // check email/mobile filled
+        // save
+        self.dismiss(animated: true, completion:nil)
+        _ = self.navigationController?.popViewController(animated: true)
     }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
@@ -54,19 +27,32 @@ class AddContactViewController: UIViewController, UITableViewDelegate, UITableVi
         return true
     }
     
-    func keyboardCancelButtonTapped(_ textField: UITextField) {
-        textField.text = ""
+    func keyboardCancelButtonTapped() {
+        mobileTextField.text = ""
+        mobileTextField.resignFirstResponder()
     }
     
-    func keyboardDoneButtonTapped(_ textField: UITextField) {
-        textField.resignFirstResponder()
+    func keyboardDoneButtonTapped() {
+        mobileTextField.resignFirstResponder()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         self.title = "Add Person"
-        self.navigationController?.navigationBar.topItem?.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil);
+        self.navigationController?.navigationBar.topItem?.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+        saveButton.layer.cornerRadius = 8
+        nameTextField.keyboardType = UIKeyboardType.namePhonePad
+        mobileTextField.keyboardType = UIKeyboardType.numberPad
+        emailTextField.keyboardType = UIKeyboardType.emailAddress
+        
+        let toolBar = UIToolbar(frame: CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: 50))
+        toolBar.barStyle = UIBarStyle.default
+        toolBar.items = [
+            UIBarButtonItem(title: "Cancel", style: UIBarButtonItemStyle.plain, target: nil, action: #selector(self.keyboardCancelButtonTapped)),
+            UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.flexibleSpace, target: nil, action: nil),
+            UIBarButtonItem(title: "Done", style: UIBarButtonItemStyle.plain, target: nil, action: #selector(self.keyboardDoneButtonTapped))]
+        toolBar.sizeToFit()
+        mobileTextField.inputAccessoryView = toolBar
     }
     
     override func didReceiveMemoryWarning() {
