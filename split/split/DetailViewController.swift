56f082e69d6f654c26e6d3f50f80a1061272d40b
Elaine Guo <YilinGUO>
2016-10-29 01:36:40 -0400

    kuaixiewanle

--- split/split/DetailViewController.swift
+++ split/split/DetailViewController.swift
@@ -9,62 +9,83 @@
 import UIKit
 
 class DetailViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, DataEnteredDelegate {
-
+	
+	@IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var tableView: UITableView!
 	@IBOutlet weak var scrollView: UIScrollView!
     var containerView = UIView()
-	var start = 20
-	var width = 80
+	var start = 10
+	var width = 50
+	var height = 50
+	var margin = 10
+	var sep = 10
 	
+	let addButton = UIButton()
     var array: Array<Dictionary<String, String>> = []
-    
+	
+//	var totalDict:Dictionary<UIButton, Double> = [:]
+	var itemArray: Array<Set<UIButton> > = []
+	var buttonArray: Array<UIButton> = []
+	var curHighlightButton: UIButton? = nil
+	var curSum: Double = 0.0
+	
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return array.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let row = indexPath.row
         let cell = tableView.dequeueReusableCell(withIdentifier: "tableCell", for: indexPath as IndexPath)
         let item : UILabel = self.view.viewWithTag(1) as! UILabel;
         let price : UILabel = self.view.viewWithTag(2) as! UILabel;
-        item.text = array[indexPath.row]["name"]
-        price.text = "$" + array[indexPath.row]["price"]!
+        item.text = array[row]["name"]
+        price.text = "$" + array[row]["price"]!
+		if (curHighlightButton != nil && itemArray[indexPath.row].contains(curHighlightButton!)) {
+			tableView.selectRow(at: indexPath, animated: false, scrollPosition: UITableViewScrollPosition.none)
+			curSum += (array[row]["price"]! as NSString).doubleValue / Double(itemArray[row].count)
+			if (curSum < 0) {
+				totalLabel.text = String(format:"Total: $0.00")
+			}
+			else {
+				totalLabel.text = String(format:"Total: $%.2f", curSum)
+			}
+		}
         return cell
     }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-		
-		self.title = "Bill Details"
-        self.navigationController?.navigationBar.topItem?.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil);
-		
-        // Do any additional setup after loading the view.
-		//containerView.backgroundColor = UIColor.yellow
-		scrollView.backgroundColor = UIColor.lightGray.withAlphaComponent(0.2)
-		
-		let addButton = UIButton()
-		//addButton.backgroundColor   = UIColor.green
-		addButton.frame = CGRect(x: start, y: 10, width: 50, height: 50)
-		addButton.setTitle("+", for: UIControlState.normal)
-		let cameraImage = UIImage(named:"assets/add.png")?.withRenderingMode(.automatic)
-		addButton.setImage(cameraImage, for:.normal)
-
-		self.containerView.addSubview(addButton)
-		start = start + width
-		addButton.addTarget(self, action: #selector(DetailViewController.addContact(_:)), for: UIControlEvents.touchUpInside)
-		//Button.addTarget(self, action: "Action:", forControlEvents: UIControlEvents.TouchUpInside)
-		//}
-		
-		//self.scrollView.delegate = self
-		self.containerView.frame = CGRect(x: 0, y: 0, width: start, height: 70)
-		self.scrollView.contentSize = CGSize(width: start, height: 70)
-		self.automaticallyAdjustsScrollViewInsets = false
-		scrollView.addSubview(containerView)
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+	
+	func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		let row = indexPath.row
+		if (curHighlightButton != nil) {
+			itemArray[row].insert(curHighlightButton!)
+			curSum += (array[row]["price"]! as NSString).doubleValue / Double(itemArray[row].count)
+		}
+		else {
+			curSum += (array[row]["price"]! as NSString).doubleValue
+		}
+		if (curSum < 0) {
+			totalLabel.text = String(format:"Total: $0.00")
+		}
+		else {
+			totalLabel.text = String(format:"Total: $%.2f", curSum)
+		}
+	}
+	
+	func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+		let row = indexPath.row
+		if (curHighlightButton != nil) {
+			curSum -= (array[row]["price"]! as NSString).doubleValue / Double(itemArray[row].count)
+			itemArray[row].remove(curHighlightButton!)
+		}
+		else {
+			curSum -= (array[row]["price"]! as NSString).doubleValue
+		}
+		if (curSum < 0) {
+			totalLabel.text = String(format:"Total: $0.00")
+		}
+		else {
+			totalLabel.text = String(format:"Total: $%.2f", curSum)
+		}
+	}
 	
 	func addContact(_ sender: UIButton) {
         print ("button to addcontact clicked")
@@ -78,17 +99,76 @@ class DetailViewController: UIViewController, UITableViewDataSource, UITableView
 		}
 	}
 	
+	func changeContact(_ sender: UIButton) {
+		print ("contact changed")
+		curHighlightButton?.layer.backgroundColor = UIColor.gray.cgColor
+		sender.layer.backgroundColor = UIColor(red: 39.0/255.0, green: 78.0/255.0, blue: 192.0/255.0, alpha: 1.0).cgColor
+		curHighlightButton = sender
+		curSum = 0.0
+		tableView.reloadData()
+	}
+	
 	func userDidEnterInformation(info: String) {
 		//label.text = info
 		print(info)
 		let usrButton = UIButton();
-		usrButton.frame = CGRect(x: start, y: 10, width: 50, height: 50)
-		start = start + width
+		//usrButton.setTitleColor(UIColor.black, for: UIControlState.normal)
+		usrButton.frame = CGRect(x: start, y: margin, width: width, height: height)
+		usrButton.layer.cornerRadius = 25
+		usrButton.layer.backgroundColor = UIColor.gray.cgColor
+//		usrButton.showsTouchWhenHighlighted = true
+		usrButton.addTarget(self, action: #selector(DetailViewController.changeContact(_:)), for: UIControlEvents.touchUpInside)
 		usrButton.setTitle(info, for: UIControlState.normal)
+		start += width + sep
+		
+		addButton.frame = CGRect(x: start, y: margin, width: width, height: height)
+		
+		buttonArray.append(usrButton)
+		if (buttonArray.count == 1) {
+			changeContact(usrButton)
+		}
 		
 		self.containerView.addSubview(usrButton)
-		self.containerView.frame = CGRect(x: 0, y: 0, width: start, height: 70)
-		self.scrollView.contentSize = CGSize(width: start, height: 70)
+		self.containerView.frame = CGRect(x: 0, y: 0, width: start + width + sep, height: 70)
+		self.scrollView.contentSize = CGSize(width: start + width + sep, height: 70)
+		if (self.containerView.frame.width > self.view.frame.width) {
+//			DispatchQueue.main.async {
+			UIView.animate(withDuration: 0.2, delay: 0, options: UIViewAnimationOptions.curveEaseOut,animations: {
+					self.scrollView.contentOffset.x = self.containerView.frame.width - self.view.frame.width
+				}, completion: nil)
+//			}
+		}
 	}
-
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		self.title = "Bill Details"
+		self.navigationController?.navigationBar.topItem?.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil);
+		
+		// Do any additional setup after loading the view.
+		// containerView.backgroundColor = UIColor.yellow
+		itemArray = Array(repeating: Set<UIButton>(), count: array.count)
+		scrollView.backgroundColor = UIColor.lightGray.withAlphaComponent(0.2)
+		
+		addButton.frame = CGRect(x: start, y: margin, width: width, height: height)
+		addButton.setTitle("+", for: UIControlState.normal)
+		let cameraImage = UIImage(named:"assets/add.png")?.withRenderingMode(.automatic)
+		addButton.setImage(cameraImage, for:.normal)
+		
+		self.containerView.addSubview(addButton)
+//		start = start + width
+		addButton.addTarget(self, action: #selector(DetailViewController.addContact(_:)), for: UIControlEvents.touchUpInside)
+		
+		//self.scrollView.delegate = self
+		self.containerView.frame = CGRect(x: 0, y: 0, width: start + width + sep, height: 70)
+		self.scrollView.contentSize = CGSize(width: start + width + sep, height: 70)
+		self.automaticallyAdjustsScrollViewInsets = false
+		scrollView.addSubview(containerView)
+	}
+	
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
 }
