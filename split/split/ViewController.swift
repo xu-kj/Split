ce1c0fc1e12aafe4da67ff3b4a414ca065c9df94
Kejia Xu <kejiaxu@umich.edu>
2016-11-06 22:55:48 -0500

    Add alert for missing device

--- split/split/ViewController.swift
+++ split/split/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-
+    
     @IBOutlet weak var cameraButton: UIButton!
     @IBOutlet weak var albumButton: UIButton!
     @IBOutlet weak var editButton: UIButton!
@@ -20,12 +20,19 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         if UIImagePickerController.isSourceTypeAvailable(.camera) {
             let picker = UIImagePickerController()
             picker.delegate = self
-            picker.sourceType = UIImagePickerControllerSourceType.camera
+            picker.sourceType = .camera
             picker.allowsEditing = true
+            
+            let screenSize: CGRect = UIScreen.main.bounds
+            let cameraAspectRatio = 4.0 / 3.0
+            let imageWidth = screenSize.width * CGFloat(cameraAspectRatio)
+            let scale = screenSize.height / CGFloat(imageWidth)
+            picker.cameraViewTransform = CGAffineTransform(scaleX: scale, y: scale)
+            
             self.present(picker, animated: true, completion: nil)
         }
         else {
-            print("can't find camera")
+            alertNoDevice(deviceName: "camera")
         }
     }
     
@@ -33,13 +40,24 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
             let picker = UIImagePickerController()
             picker.delegate = self
-            picker.sourceType = UIImagePickerControllerSourceType.photoLibrary
-            picker.allowsEditing = true
+            picker.sourceType = .photoLibrary
+            picker.allowsEditing = false
             self.present(picker, animated: true, completion: nil)
         }
         else {
-            print("can't find album")
+            alertNoDevice(deviceName: "album")
         }
+    }
+    
+    func alertNoDevice(deviceName: String){
+        let alertVC = UIAlertController(title: "Sorry!",
+                                        message: "Cannot access " + deviceName,
+                                        preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK",
+                                     style:.default,
+                                     handler: nil)
+        alertVC.addAction(okAction)
+        present(alertVC, animated: true, completion: nil)
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?) {
@@ -49,16 +67,16 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         self.performSegue(withIdentifier: "ToTable", sender: self)
     }
     
-	override func viewDidLoad() {
-		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
         let cameraImage = UIImage(named:"assets/camera.png")?.withRenderingMode(.automatic)
         cameraButton.setImage(cameraImage, for:.normal)
         let albumImage = UIImage(named:"assets/album.png")?.withRenderingMode(.automatic)
         albumButton.setImage(albumImage, for:.normal)
         let editImage = UIImage(named:"assets/edit.png")?.withRenderingMode(.automatic)
         editButton.setImage(editImage, for:.normal)
-	}
+    }
     
     override func viewDidAppear(_ animated: Bool) {
         let imageView = UIImageView(frame: CGRect(x: 0, y:0, width: self.view.frame.width, height: 35))
@@ -69,10 +87,10 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         self.navigationController?.navigationBar.topItem?.titleView = imageView
     }
     
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
-	}
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if (segue.identifier == "ToTable") {
