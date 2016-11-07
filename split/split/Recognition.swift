f947abae41b0882d1d051616f8c6d56cf2253c87
cytoplazm <guoyilin@hotmail.com>
2016-11-06 19:18:37 -0500

    关listener

--- split/split/Recognition.swift
+++ split/split/Recognition.swift
@@ -22,7 +22,22 @@ class Recognition: NSObject, G8TesseractDelegate {
 			if line != "" {
 				var fullNameArr  = line.components(separatedBy: " ")
 				if fullNameArr[fullNameArr.count - 1] == "T" || fullNameArr[fullNameArr.count - 1] == "F" {
+					
+					
 					let price = fullNameArr[fullNameArr.count - 2]
+//					var price = ""
+//					print(line)
+//					for char in line.characters.reversed() {
+//						if (char >= "0" && char <= "9") {
+//							price = "\(char)" + price
+//						}
+//						if char == "." {
+//							price = "." + price
+//							break
+//						}
+//					}
+					
+					
 					fullNameArr.remove(at: fullNameArr.count - 1)
 					fullNameArr.remove(at: fullNameArr.count - 1)
 					let name =  fullNameArr.joined(separator: " ")
